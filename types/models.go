3a199467cc2c720f3e3dde08ad428b4cd1a8fbb9
simpleCalculus <60369311+simpleCalculus@users.noreply.github.com>
2022-06-08 06:40:17 +0500

    Create models.go

---
+++ types/models.go
@@ -0,0 +1,25 @@
+package types
+
+type Update struct {
+	UpdateID int     `json:"update_id"`
+	Message  Message `json:"message"`
+}
+
+type Message struct {
+	Chat Chat   `json:"chat"`
+	Text string `json:"text"`
+}
+
+type Chat struct {
+	ChatID int `json:"id"`
+}
+
+type RestResponse struct {
+	Result []Update `json:"result"`
+}
+
+type BotMessage struct {
+	ChatID    int    `json:"chat_id"`
+	Text      string `json:"text"`
+	Animation []byte `json:"animation"`
+}
